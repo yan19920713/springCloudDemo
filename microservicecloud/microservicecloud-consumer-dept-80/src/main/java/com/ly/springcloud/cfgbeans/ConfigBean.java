4bcb5798a72baa656ffe975a91ff72d34001b12c
Mr.Yan <yan_19920713@163.com>
2019-07-11 14:47:32 +0800

    Ribbon

--- microservicecloud/microservicecloud-consumer-dept-80/src/main/java/com/ly/springcloud/cfgbeans/ConfigBean.java
+++ microservicecloud/microservicecloud-consumer-dept-80/src/main/java/com/ly/springcloud/cfgbeans/ConfigBean.java
@@ -1,5 +1,9 @@
 package com.ly.springcloud.cfgbeans;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import com.netflix.loadbalancer.RetryRule;
+import com.netflix.loadbalancer.RoundRobinRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,7 +23,16 @@ public class ConfigBean {
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
+
+    @Bean
+    public IRule myRule() {
+//        return new RoundRobinRule() // 轮询
+//        return new RandomRule();// 随机：达到的目的，用我们重新选择的随机算法替代默认的轮询。（原来负载均衡是默认轮询）
+        return new RetryRule();//重试
+    }
 }
+
+
 //@Bean
 //public UserServcie getUserServcie()
 //{
