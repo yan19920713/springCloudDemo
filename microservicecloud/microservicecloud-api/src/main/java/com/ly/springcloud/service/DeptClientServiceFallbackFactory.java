53f926613242c82529f54d66f44326adfdd0ac0b
Mr.Yan <yan_19920713@163.com>
2019-07-11 17:59:34 +0800

    Hystrix

---
+++ microservicecloud/microservicecloud-api/src/main/java/com/ly/springcloud/service/DeptClientServiceFallbackFactory.java
@@ -0,0 +1,10 @@
+package com.ly.springcloud.service;
+
+/**
+ * @program: microservicecloud
+ * @description:
+ * @author: Mr.Yan
+ * @create: 2019-07-11
+ **/
+public interface DeptClientServiceFallbackFactory {
+}
