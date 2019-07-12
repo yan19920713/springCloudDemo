1c7c9aed5b37b2a2d2137cae4f4b69162bbf94bb
Mr.Yan <yan_19920713@163.com>
2019-07-12 13:57:40 +0800

    HystrixDashboard

--- microservicecloud/microservicecloud-api/src/main/java/com/ly/springcloud/service/DeptClientService.java
+++ microservicecloud/microservicecloud-api/src/main/java/com/ly/springcloud/service/DeptClientService.java
@@ -11,10 +11,13 @@ import java.util.List;
 /**
  * @program: microservicecloud
  * @description:
+ * @Description: 修改microservicecloud-api工程，根据已经有的DeptClientService接口
+                   新建 一个实现了FallbackFactory接口的类DeptClientServiceFallbackFactory
  * @author: Mr.Yan
  * @create: 2019-07-11
  **/
-@FeignClient(value = "MICROSERVICECLOUD-DEPT")
+//@FeignClient(value = "MICROSERVICECLOUD-DEPT")
+@FeignClient(value = "MICROSERVICECLOUD-DEPT",fallbackFactory=DeptClientServiceFallbackFactory.class)
 public interface DeptClientService {
 
     @RequestMapping(value = "/dept/get/{id}", method = RequestMethod.GET)
