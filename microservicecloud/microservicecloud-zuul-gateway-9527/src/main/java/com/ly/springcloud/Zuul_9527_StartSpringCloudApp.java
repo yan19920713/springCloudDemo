c9078be619d6d4bf5f4189c432e6ddc503071968
Mr.Yan <yan_19920713@163.com>
2019-07-12 15:22:11 +0800

    Zuul 网关

---
+++ microservicecloud/microservicecloud-zuul-gateway-9527/src/main/java/com/ly/springcloud/Zuul_9527_StartSpringCloudApp.java
@@ -0,0 +1,19 @@
+package com.ly.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @program: microservicecloud
+ * @description:
+ * @author: Mr.Yan
+ * @create: 2019-07-12
+ **/
+@SpringBootApplication
+@EnableZuulProxy
+public class Zuul_9527_StartSpringCloudApp {
+    public static void main(String[] args) {
+        SpringApplication.run(Zuul_9527_StartSpringCloudApp.class, args);
+    }
+}
